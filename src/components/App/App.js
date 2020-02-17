451368ed4880c8c304387f5ea5091dcc76989c56
nagireddy panditi <nagireddy.panditi@gmail.com>
2020-02-17 20:47:27 +0530

    first commit

---
+++ src/components/App/App.js
@@ -0,0 +1,39 @@
+import React from "react"
+import ImageList from "../ImageList/ImageList"
+import ImageSearch from "../ImageSearch/ImageSearch"
+import {API_TOKEN} from '../../utils'
+
+// console.log(process.env.REACT_APP_API_TOKEN)
+
+class App extends React.Component {
+
+  state = {
+    images: [],
+    error: null
+  }
+  handleMakeRequest = async (e) => {
+    e.preventDefault()
+    const searchValue = e.target.elements.searchValue.value
+    const request = await fetch(`https://pixabay.com/api/?key=${API_TOKEN}&q=${searchValue}&per_page=15`)
+    const results = await request.json()
+    if (searchValue) {
+      this.setState({ images: results.hits, error: null })
+    } else {
+      this.setState({ error: "Please provide a value." })
+    }
+  }
+  render() {
+    return (
+      <div>
+        <ImageSearch handleMakeRequest={this.handleMakeRequest} />
+        { 
+          this.state.error !== null ? 
+          <div style={{ color:"#fff", textAlign:"center" }}>{ this.state.error }</div> : 
+          <ImageList images={this.state.images} /> 
+        }
+      </div>
+    )
+  }
+}
+
+export default App
---
+++ src/utils.js
@@ -0,0 +1,1 @@
+export const API_TOKEN = "15289865-4d4735ad9d843b3939dbb5173"
